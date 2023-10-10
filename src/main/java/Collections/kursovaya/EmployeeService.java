4b607771c78065a91793d8e7611218e3199ec464
Denis <tequila96@bk.ru>
2023-10-10 16:11:46 +0500

    Начало

---
+++ src/main/java/Collections/kursovaya/EmployeeService.java
@@ -0,0 +1,16 @@
+package Collections.kursovaya;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+
+
+public class EmployeeService {
+
+    List<String> employees = new ArrayList<>();
+
+
+}
